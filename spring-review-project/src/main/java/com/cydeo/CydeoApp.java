77e00e4e9b124eff9cda791f69ddc3e8234ef788
Rustam Jafarov <rustam.jafarov@empic.de>
2023-09-20 21:50:09 +0200

    Spring Boot review-2

---
+++ spring-review-project/src/main/java/com/cydeo/CydeoApp.java
@@ -0,0 +1,16 @@
+package com.cydeo;
+
+import com.cydeo.config.EmployeeConfig;
+import com.cydeo.service.SalaryService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+
+    public static void main(String[] args) {
+        ApplicationContext container = new AnnotationConfigApplicationContext(EmployeeConfig.class);
+        SalaryService salaryService = container.getBean(SalaryService.class);
+
+        salaryService.calculateRegularSalary();
+    }
+}
---
+++ spring-review-project/src/main/java/com/cydeo/service/SalaryService.java
@@ -0,0 +1,24 @@
+package com.cydeo.service;
+
+import com.cydeo.repository.EmployeeRepository;
+import com.cydeo.repository.HoursRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SalaryService {
+
+    EmployeeRepository employeeRepository;
+    HoursRepository hoursRepository;
+
+    public SalaryService(EmployeeRepository employeeRepository,@Qualifier ("RH") HoursRepository hoursRepository) {
+        this.employeeRepository = employeeRepository;
+        this.hoursRepository = hoursRepository;
+    }
+
+    public void calculateRegularSalary(){
+
+        //hourlyRate * Regular hours
+        System.out.println(employeeRepository.getHourlyRate() * hoursRepository.getHours());
+    }
+}
